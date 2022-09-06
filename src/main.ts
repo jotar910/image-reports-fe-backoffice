abd95ed152e2b8c3443ad751e0055b76beda444a
João Paulo Rodrigues <jro@container-xchange.com>
2022-09-06 15:37:40 +0100

    remove unnecessary store

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,8 @@ import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 
 createApp(App)
-  .use(store)
   .use(router)
   .use(PrimeVue, { ripple: true })
   .use(ToastService)
